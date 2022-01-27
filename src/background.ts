23439e724079b50f6fce791deafca8590d70e8fe
enis <enis@codeshake.io>
2022-01-28 00:26:00 +0300

    Initial commit

---
+++ src/background.ts
@@ -0,0 +1,15 @@
+let active = false;
+
+function makeOrange(color: string): void {
+    document.body.style.backgroundColor = color;
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    active = !active;
+    const color = active ? 'orange' : 'white';
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id ? tab.id : -1},
+        func: makeOrange,
+        args: [color]
+    }).then();
+});
